1618e5526c79131f5d39bef7b81b69a7a83e3ce2
Juakoo2103 <joaquineduardo.gonzalez@gmail.com>
2026-05-28 11:51:06 -0400

    Agrega footer de derechos reservados

--- src/App.jsx
+++ src/App.jsx
@@ -413,6 +413,24 @@ function App() {
           </Stack>
         </Box>
 
+        <Box
+          component="footer"
+          sx={{
+            pt: 4,
+            pb: 1,
+          }}
+        >
+          <Typography
+            variant="caption"
+            sx={{
+              color: "text.secondary",
+              fontWeight: 500,
+            }}
+          >
+            Derechos reservados juakooDEV
+          </Typography>
+        </Box>
+
         {/* Toast notifications */}
         <Toaster />
       </Container>
